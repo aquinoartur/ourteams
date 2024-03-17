cd6e63a78b9bb99f1c8283428597c055f9626830
adelmo aquino <80642826+aquinoartur@users.noreply.github.com>
2024-03-17 16:49:44 -0300

    init

---
+++ src/home/home.tsx
@@ -0,0 +1,13 @@
+
+import { Text, View } from 'react-native';
+import { styles } from './home_style';
+
+export function Home() {
+ return (
+  <View style={styles.container}>
+   <Text>Home</Text>
+  </View>
+ );
+}
+
+
---
+++ src/home/home_style.ts
@@ -0,0 +1,12 @@
+
+import { StyleSheet } from 'react-native';
+
+
+export const styles = StyleSheet.create({
+ container: {
+  flex: 1,
+  backgroundColor: '#81b1e3',
+  alignItems: 'center',
+  justifyContent: 'center',
+ },
+});
