0a234ebf8c4d3f2282013f95120206b51edd66d7
adelmo aquino <80642826+aquinoartur@users.noreply.github.com>
2024-03-18 19:49:10 -0300

    loading fonts

--- App.js
+++ App.js
@@ -5,14 +5,20 @@ import { StatusBar } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { Home } from 'src/app/home/home';
 import { LoadingIndicator } from '@widgets/loading';
-
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
 export default function App() {
+
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold
+  });
+
   return (
     <SafeAreaProvider>
       <ThemeProvider theme={theme} >
         <StatusBar barStyle="light-content" backgroundColor="transparent" translucent />
-        {true ? <Home /> : <LoadingIndicator />}
+        {fontsLoaded ? <Home /> : <LoadingIndicator />}
       </ThemeProvider>
     </SafeAreaProvider>
   );
--- src/app/home/home.tsx
+++ src/app/home/home.tsx
@@ -1,12 +1,13 @@
 import { Header } from '@widgets/header';
 import * as HS from './home_style';
 import { View } from 'react-native';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
 export function Home() {
-
+ const insets = useSafeAreaInsets();
  return (
   <HS.Background>
-   <View style={{ marginTop: 50 }}>
+   <View style={{ marginTop: insets.top }}>
     <Header />
    </View>
   </HS.Background >
